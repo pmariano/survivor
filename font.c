4dfbfa0d689bbefcefdcdb866f9990f35cbbba45
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2012-09-02 03:18:11 -0300

    caching font

--- font.c
+++ font.c
@@ -1,14 +1,18 @@
 #include "font.h"
 
-extern unsigned char adler_ttf[];
-extern unsigned int adler_ttf_len;
+#define TTF_POINT_LIMIT 99
+TTF_Font *ttf_point_cache[TTF_POINT_LIMIT];
 
 void init_font() {
     TTF_Init();
+	memset(ttf_point_cache, 0, sizeof(ttf_point_cache));
 }
 
-TTF_Font *setup_ttf_adler(int points){
-    TTF_Font *ttf_tmp = TTF_OpenFontRW(SDL_RWFromMem(adler_ttf, adler_ttf_len), 1, points);
+TTF_Font *setup_ttf(int points){
+	if(points >= TTF_POINT_LIMIT) return NULL;
+    TTF_Font *ttf_tmp = ttf_point_cache[points];
+	if(ttf_tmp) return ttf_tmp;
+    ttf_point_cache[points] = ttf_tmp = TTF_OpenFont("data/Chalkduster.ttf", points);
 
     int renderstyle = TTF_STYLE_NORMAL;
     int outline = 0;
@@ -32,7 +36,7 @@ void text_write(SDL_Surface *screen, int x, int y, char *message, int selected){
 void text_write_raw(SDL_Surface *screen, int x, int y, char *message, SDL_Color color, int points) {
     TTF_Font *tmp;
 
-    tmp = setup_ttf_adler(points);
+    tmp = setup_ttf(points);
     SDL_Rect dstrect;
     SDL_Surface *text;
 
