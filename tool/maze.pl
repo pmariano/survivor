063a00e986650b19230b399d4a62669863a920cb
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2012-09-16 13:43:16 -0300

    random maze generation + Graph::SP_Dijkstra + GA

--- tool/maze.pl
+++ tool/maze.pl
@@ -1,267 +1,10 @@
-#!/usr/bin/perl
-
-#------------------------------------------------------------------------
-#    Copyright (C) 2011 Luca Amore <luca.amore at gmail.com>
-#
-#    Maze is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Maze is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Maze.  If not, see <http://www.gnu.org/licenses/>.
-#------------------------------------------------------------------------
-
 use strict;
 use warnings;
 
-package Maze;
-
-use Carp qw(croak verbose);
-use GD;
-
-sub new {
-    my ($class, $x, $y) = @_; 
-
-    my $self = { 
-        x => $x, 
-        y => $y,
-        door => [], 
-        solution => [],
-    };
-
-    bless $self;
-    $self->openWall(0, 0, 'N');
-    $self->openWall($x - 1, $y - 1, 'S');
-
-    return $self;
-}
-
-sub _getWallIndex($$$){
-    my ($self, $x, $y, $dir) = @_;
-
-    my ($wx, $wy, $wdir) = 
-        $dir eq 'N' ? ($x,      $y,     0) :
-        $dir eq 'S' ? ($x,      $y + 1, 0) :
-        $dir eq 'W' ? ($x,      $y,     1) :
-        $dir eq 'E' ? ($x + 1,  $y,     1) :
-            croak "wrong direction";
-
-    return ($wy*$self->{x}+$wx)*2+$wdir;
-}
-
-sub isWallOpen($$$){
-    my ($self, $x, $y, $dir) = @_;
-
-    my $idx = $self->_getWallIndex($x, $y, $dir);    
-
-    return $self->{door}[$idx] || 0;
-}
-
-sub openWall($$$) {
-    my ($self, $x, $y, $dir) = @_;
-
-    my $idx = $self->_getWallIndex($x, $y, $dir);    
-
-    $self->{door}[$idx] = 1;
-}
-
-sub getCellNeighbors($$$){
-    my ($self, $x, $y) = @_;
-    grep {
-        $_->[0] >= 0 and $_->[0] < $self->{x} and
-        $_->[1] >= 0 and $_->[1] < $self->{y}
-    } (
-        [$x - 1, $y    , 'W'],
-        [$x + 1, $y    , 'E'],
-        [$x    , $y - 1, 'N'],
-        [$x    , $y + 1, 'S']
-    );
-}
-
-sub getCellOpenedNeighbors($$$){
-    my ($self, $x, $y) = @_;
-    grep { 
-        my (undef, undef, $dir) = @$_;
-        $self->isWallOpen($x, $y, $dir)
-    } $self->getCellNeighbors($x, $y);
-}
-
-sub isCellExit($$$){
-    my ($self, $x, $y) = @_;
-    return 
-        ($x == $self->{x} -1 ) && ($y == $self->{y} -1 );
-} 
-
-sub markSolution($$$$){
-    my ($self, $x, $y, $id) = @_;
-    $self->{solution}[$x][$y] |= 1 << $id;
-}
-
-sub isSolution($$$){
-    my ($self, $x, $y) = @_;
-    $self->{solution}[$x][$y];
-}
-
-# generate maze
-sub asterione($$$;$){
-    no warnings 'recursion';
-    my ($self, $x, $y, $x1, $y1, $visited) = @_;
-    $visited->[$x][$y] = 1;
-    return if $x == $x1 && $y == $y1;
-    my @neighbors = $self->getCellNeighbors($x, $y);
-    while (scalar @neighbors){
-        my ($tox, $toy, $dir) = 
-            @{ splice(@neighbors, rand(@neighbors), 1) };
-        next if $visited->[$tox][$toy];
-        $self->openWall($x, $y, $dir);
-        $self->asterione($tox, $toy, $x1, $y1, $visited);
-    }
-}
-
-# solve maze
-sub teseo($$$$$;$){
-    no warnings 'recursion';
-    my ($self, $x, $y, $x1, $y1, $id, $visited) = @_;
-    $visited->[$x][$y] = 1;
-    # if ($self->isCellExit($x, $y)){
-    if ($x == $x1 && $y == $y1){
-        $self->markSolution($x, $y, $id);
-        return 1;
-    }
-    my @neighbors = $self->getCellOpenedNeighbors($x, $y);
-    while (scalar @neighbors){
-        my ($tox, $toy, $dir) = 
-            @{ splice(@neighbors, rand(@neighbors), 1) };
-        next if $visited->[$tox][$toy];
-        my $isSolution = $self->teseo($tox, $toy, $x1, $y1, $id, $visited);
-        if ($isSolution){
-            $self->markSolution($x, $y, $id);
-            return $isSolution+1;
-        }
-    }
-    return 0;
-}
-
 use Term::ANSIColor;
-
-sub toText(){
-
-    my $self = shift;
-
-    my ($x, $y, @l1, @l2);
-    my $out = '';
-    for ($y = 0; $y < $self->{y}; $y++){
-        @l1 = @l2 = ();
-        for ($x = 0; $x < $self->{x}; $x++){
-            my $sol = $self->isSolution($x, $y) || 0;
-			my $overload = unpack '%b*', pack 'I', $sol;
-			my @color = qw(black blue cyan green yellow red magenta);
-            my $solution = color($color[$overload]) .
-				sprintf("%02x", $sol). 
-				color('reset');
-            push(@l1, $self->isWallOpen($x, $y, 'N') ? $solution : '-' x 2);
-            push(@l2, $self->isWallOpen($x, $y, 'W') ? ' ' : '|');
-            push(@l2, $solution);
-        }
-        push(@l2, $self->isWallOpen($x, $y, 'E') ? ' ' : '|');
-        $out .= 
-            '+' . join('+',@l1) . '+' . "\n" . 
-            join('',@l2) . "\n";
-    }
-
-    @l1 = ();
-    for ($x = 0; $x < $self->{x}; $x++){
-        my $solution = $self->{solution}[$x][$self->{y} -1] ? '.' : ' ';
-        push(@l1, 
-            $self->isWallOpen($x, $self->{y} -1, 'S') ? 
-                $solution x 2 : '-' x 2
-        );
-    }
-
-    $out .= '+' . join('+', @l1) . '+' . "\n";
-
-    print $out;
-}
-
-sub toImage($$){
-
-    my ($self, $FILENAME) = @_;
-
-    my ($WX, $WY) = (10, 10);
-
-    my ($SIZEX, $SIZEY) = ($self->{x} * $WX, $self->{y} * $WY);
-
-    my $img = new GD::Image->newTrueColor($SIZEX,$SIZEY)
-        or croak "Can't create GD::Image";
- 
-    my $cl_white = $img->colorAllocate(255,255,255);
-    my $cl_black = $img->colorAllocate(  0,  0,  0);
-    my $cl_red   = $img->colorAllocate(255,  0,  0);
-    
-    $img->fill( 0, 0, $cl_white);
-
-    open(my $fh, '>', $FILENAME)
-        or croak "Can't open $FILENAME: $!";
-
-    binmode $fh;
-
-    my ($xx, $yy);
-
-    YY: for ($yy = 0; $yy < $self->{y}; $yy++){
-
-        XX: for ($xx = 0; $xx < $self->{x}; $xx++){
-
-            $img->filledRectangle(
-                $xx * $WX, $yy * $WY, ($xx + 1) * $WX, ($yy + 1) * $WY, 
-                $cl_red
-            )
-                if $self->isSolution($xx, $yy);
-            
-            $img->line(
-                    $xx * $WX, $yy * $WY, ($xx + 1) * $WX, $yy * $WY, 
-                    $cl_black
-            )
-                unless $self->isWallOpen($xx, $yy, 'N');
-
-            $img->line(
-                    $xx * $WX, $yy * $WY, $xx * $WX, ($yy + 1) * $WY, 
-                    $cl_black
-            )
-                unless $self->isWallOpen($xx, $yy, 'W');
-        }
-
-        $img->line(
-            $xx * $WX - 1, $yy * $WY, $xx * $WX -1, ($yy + 1) * $WY, 
-            $cl_black
-        )
-            unless $self->isWallOpen($xx - 1, $yy, 'E');
-    }
-
-    for ($xx = 0; $xx < $self->{x}; $xx++){
-        $img->line(
-            $xx * $WX, $yy * $WY - 1, ($xx + 1) * $WX, $yy * $WY - 1, 
-            $cl_black
-    )
-                unless $self->isWallOpen($xx, $yy - 1, 'S');
-    }
-
-    print $fh $img->png(0);
-    close $fh;
-}
-
-package main;
-
-
-
-
+use Graph::Undirected;
+# use Graph::Writer::Dot;
 use Algorithm::Combinatorics qw(combinations);
-use Storable qw(dclone);
 use List::Util qw(sum);
 
 use Algorithm::Evolutionary qw( Individual::BitString
@@ -272,8 +15,8 @@ use Algorithm::Evolutionary::Utils qw(entropy consensus);
 my $pop_size = shift || 100;
 my $selection_rate = shift || 0.25;
 
-my $w = 10;
-my $h = 7;
+my $w = 22-2;
+my $h = 16-2;
 
 my @entry = (
 	[$w/2,0],
@@ -285,45 +28,115 @@ my @entry = (
 my @task = combinations([@entry],2);
 # print join ';', map {join ',', map { "@$_"}  @$_} combinations([@entry],2);
 
-my $max_score = -1;
-my $winner = {};
+# my $writer = Graph::Writer::Dot->new();
 
-my $bit_count = $w*$h*2;
-my @pop;
+my $bit_count = $w*$h;
+my @pop = (
+	# Algorithm::Evolutionary::Individual::BitString->fromString( '0' x $bit_count ),
+);
 for (1..$pop_size) {
 	push @pop, Algorithm::Evolutionary::Individual::BitString->new($bit_count);
-
 }
 # use Data::Dumper; print Dumper \@pop;
 
-my $m = Algorithm::Evolutionary::Op::Bitflip->new(1);
-my $c = Algorithm::Evolutionary::Op::Crossover->new(2,4);
+my $m = Algorithm::Evolutionary::Op::Bitflip->new(4);
+my $c = Algorithm::Evolutionary::Op::Crossover->new(4);
+
+sub idx {
+	my ($x, $y) = @_;
+	$x+$y*$w
+}
+
+sub coord {
+	my ($i) = @_;
+	($i % $w, int($i / $w))
+}
+
+sub val($$$) {
+	my ($ind, $x, $y) = @_;
+	$ind->Atom(idx($x,$y))
+}
 
 my $fitness = sub {
-	my $indi = shift;
+	my $ind = shift;
 	my $print = shift;
 
-	my @door = map {$indi->Atom($_)} 0..$indi->size()-1;
+	my @grid = ();
+	for (my $y=0; $y<$h; $y++) { 
+		for (my $x=0; $x<$w; $x++) { 
+			$grid[$x][$y] = val($ind,$x,$y);
+			# print "$x $y: $grid[$x][$y]\n" if $print;
+		}
+	}
 
-	my $map = Maze->new($w,$h);
-	$map->{door} = \@door;
+	my $graph = Graph::Undirected->new;
+	for (my $y=0; $y<$h; $y++) { 
+		for (my $x=0; $x<$w; $x++) { 
+			my $i0 = idx($x,$y);
+			my $v0 = $ind->Atom($i0);
+			if($x+1<$w) {
+				my $i1 = idx($x+1,$y);
+				my $v1 = $ind->Atom($i1);
+				$graph->add_edge($i0, $i1) unless $v0 || $v1; 
+			}
 
-	my $id = 0;
+			if($y+1<$h) {
+				my $i2 = idx($x,$y+1);
+				my $v2 = $ind->Atom($i2);
+				$graph->add_edge($i0, $i2) unless $v0 || $v2; 
+			}
+		}
+	}
+
+	my $task_id = 0;
+	my @visited = ();
 	my @path = map {
 		my ($start, $end) = @$_;
-		$map->teseo(
-				$start->[0], $start->[1],
-				$end->[0], $end->[1],
-				$id++,
-				);
+		my @path = $graph->SP_Dijkstra(idx(@$start), idx(@$end));
+		for my $i (@path) {
+			my ($x, $y) = coord($i);
+			$visited[$x][$y] |= 1 << $task_id;
+		}
+		$task_id++;
+		scalar(@path)
 	} @task;
 
-	return 1 if @path != @task || grep {!$_} @path;
+	if($print) {
+		print $ind->asString(), "\n";
+		print "path lengths @path\n";
+		for (my $y=0; $y<$h; $y++) { 
+			for (my $x=0; $x<$w; $x++) { 
+				my $visit = exists $visited[$x][$y] ? $visited[$x][$y] : 0;
+				my $over = unpack '%b*', pack 'I', $visit;
+				my @color = qw(black blue cyan green yellow red magenta);
+				my $color = $grid[$x][$y] ? 'white' : $color[$over];
+				my $char = $grid[$x][$y] ? '##' : sprintf "%02x", $visit;
+				print color($color), $char;
+			}
+			print color('reset'), "\n";
+		}
+
+		# $writer->write_graph($graph, 'graph.dot');
+	}
+
+	my $openness = $bit_count;
+	for (my $y = 0; $y < $h; $y++){
+		for (my $x = 0; $x < $w; $x++){
+			$openness-- if $grid[$x][$y];
+		}
+	}
+
+	# return 1 if @path != @task || grep {!$_} @path;
+	my $should = @task;
+	my $did = grep {$_} @path;
+	return 1+$openness/$bit_count+10*$did/$should if $should != $did;
+
+	my $dissonance_pow = 1;
+	my $overload_pow = 1;
 
 	my $n = @path;
 	my $sum = sum(@path);
 	my $average = $sum/$n;
-	my $dissonance_pow = 2;
 	my @dissonance = map { abs($_ - $average) ** $dissonance_pow } @path;
 	my $pow_sum = sum(map { $_ ** $dissonance_pow } @path);
 	my $dissonance = sum(@dissonance) / $pow_sum ;
@@ -332,20 +145,16 @@ my $fitness = sub {
 	my $coverage = 0;
 	for (my $y = 0; $y < $h; $y++){
 		for (my $x = 0; $x < $w; $x++){
-			my $sol = $map->isSolution($x, $y) || 0;
-			my $over = unpack '%b*', pack 'I', $sol;
-			$overload += $over ** 2;
+			my $visit = exists $visited[$x][$y] ? $visited[$x][$y] : 0;
+			my $over = unpack '%b*', pack 'I', $visit;
+			$overload += $over ** $overload_pow;
 			$coverage ++ if $over;
 		}
 	}
 
-	my $score = 10 + $coverage ** 3 * $sum ** 2 / $dissonance / $overload ;
+	my $score = 100 + $coverage * $sum / $dissonance / $overload; # / $openness;
 
-	if($print) {
-		print $indi->asString()," $score \n";
-		print "path lengths @path\n";
-		$map->toText();
-	}
+	# print "score $score\n" if $print;
 
 	return $score;
 };
@@ -359,7 +168,6 @@ while(1) {
 	$generation->apply( \@pop );
 	print "$genCount\n";
 
-	&$fitness($pop[0], 1);
 	&$fitness($pop[0], 1);
 
 	$genCount++;
